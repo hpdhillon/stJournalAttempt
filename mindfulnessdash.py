9c40d2c3b7e0236529647b1ab784ffc22104ce7f
hpdhillon <57342519+hpdhillon@users.noreply.github.com>
2020-10-08 03:20:15 -0400

    Add files via upload

--- mindfulnessdash.py
+++ mindfulnessdash.py
@@ -98,7 +98,8 @@ if st.button('Analysis'):
     if booleon <  -2:
         st.write("You sound sad. That's fine. Let it all out.")
         st.markdown("![Alt Text](https://media.tenor.com/images/ff4a60a02557236c910f864611271df2/tenor.gif)")
-    if boolean > 2:
+        st.markdown("[Click here if you need extra help](https://suicidepreventionlifeline.org/chat/)")
+    if booleon > 2:
         st.write("You are a ray of sunshine today! Keep it up playa!")
         st.markdown("![Alt Text](https://media.tenor.com/images/2aa9b6f3a7d832c2ff1c1a406d5eae73/tenor.gif)")
 if st.button('Save as text file'):
